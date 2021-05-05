a65d3345499fb4d170dc9aa679aec0511f56790e
Никита Вальпа <Nikichvalpa@mail.ru>
2021-05-05 14:01:51 +0500

    Написал основную логику изменения объекта. Добавил отрисовку сообщения об ошибке, в случае возникновения исключения на каком-то из этапов.

--- PropertyChanger/Hubs/PropertyEditorHub.cs
+++ PropertyChanger/Hubs/PropertyEditorHub.cs
@@ -4,15 +4,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace PropertyChanger.Hubs
 {
-    public class PropertyEditorHub:Hub
+    public class PropertyEditorHub : Hub
     {
         private readonly ILogger<PropertyEditorHub> _logger;
-        private Dictionary<string, object> keyValuePairsToClient = new Dictionary<string, object>();
+        private Dictionary<string, object> propsToClient = new Dictionary<string, object>();
+        private Dictionary<string, Func<JsonElement,object>> Converter = new Dictionary<string, Func<JsonElement,object>>
+        {
+            ["SByte"] = jsonElem => { return jsonElem.GetSByte(); },
+            ["Byte"] = jsonElem => { return jsonElem.GetByte(); },
+            ["Int16"] = jsonElem => { return jsonElem.GetInt16(); },
+            ["UInt16"] = jsonElem => { return jsonElem.GetUInt16(); },
+            ["Int32"] = jsonElem => { return jsonElem.GetInt32(); },
+            ["UInt32"] = jsonElem => { return jsonElem.GetUInt32(); },
+            ["Int64"] = jsonElem => { return jsonElem.GetInt64(); },
+            ["UInt64"] = jsonElem => { return jsonElem.GetUInt64(); },
+        };
+        private static object _obj;
         private readonly List<string> ValidTypes = new List<string>() {
             "SByte",
             "Byte",
@@ -29,7 +42,7 @@ namespace PropertyChanger.Hubs
             _logger = logger;
         }
 
-        private PropertyInfo[] Parser(Type type) //метод для парсинга объектов
+        private PropertyInfo[] Parser(Type type) //метод для парсинга свойств объектов
         {
             var props = new List<PropertyInfo>();
             foreach (var prop in type.GetProperties()) {
@@ -46,21 +59,78 @@ namespace PropertyChanger.Hubs
             }
             return props.ToArray();
         }
+        /*Так, тут потребуется много комментариев, поехали.
+         * Суть в следующем, от клиента нам приходит объект с ключом и значением, ключ, это имя нашего свойства, а значение, это структура JsonElement 
+         * просто приведенная к object. Так вот, нам надо взять этот JsonElement и привести его к одному из целочисленных типов данных, которое поддерживает
+         * наше приложение, для этого есть словарь конвертер. В него запиханы функции, которые как раз и приводят наш jsonElement например к int32 или int16 и т.д.
+         * А доступ к функциям конвертера мы получаем по ключу, который является названием типа. Сам тип к которому нам нужно привести jsonElement мы получаем
+         * путем сопоставления имени свойства которое к нам пришло от клиента и имени свойства в нашем .NET объекте.
+         * Конец)
+         */
+        private object NumberConverter(Dictionary<string, object> JSprops) {
+            var NETprops = Parser(_obj?.GetType());
+            foreach (var JSname in JSprops.Keys) {
+                foreach (var NETName in NETprops) {
+                    if (NETName.Name == JSname) {
+                        return Converter[NETName.PropertyType.Name]((JsonElement)JSprops[JSname]);
+                    }
+                }
+            }
+            return 0;//безопасим наше присваивание, ведь 0 поддерживают все целочисленные типы данных
+        }
 
-        public async Task Edit(object obj) {
+        public async Task Edit(object obj) {//метод в котором происходит чтение свойст объекта и отправка их клиенту для изменения
             try
             {
-                var ParsedProperties = Parser(obj.GetType());
+                _obj = obj;
+                var ParsedProperties = Parser(_obj?.GetType());
                 foreach (var prop in ParsedProperties) {
-                    keyValuePairsToClient.Add(prop.Name, prop.GetValue(obj));
+                    propsToClient.Add(prop.Name, prop.GetValue(_obj));
                 }
-                
-                await Clients?.All.SendAsync("Recieve", keyValuePairsToClient);//так как это веб приложение, то я не придумал ничего лучше, чем просто принудительно отправлять json объект нашему клиенту на js
+
+                await Clients?.All.SendAsync("Recieve", propsToClient);//так как это веб приложение, то я не придумал ничего лучше, чем просто принудительно отправлять json объект нашему клиенту на js при подключении к хабу
             }
             catch (Exception ex) {
-                _logger.LogError("При попытке парсинга свойств и отправке их клиенту произошла ошибка: " + ex.Message + " stackTrace \n" + ex.StackTrace);
+                _logger.LogError("При попытке парсинга свойств и отправке их клиенту произошла ошибка: " + ex.Message + " stackTrace \n" + ex.StackTrace);//для разработчика
+                Clients?.All.SendAsync("Error", "Ошибка!", "При чтении свойств объекта на сервере, произошла ошибка, для того чтобы попробовать снова, обновите страничку");//для клиента
             }
             
+        }
+
+        public async Task Recieve(Dictionary<string, object> props) {//метод который принимает уже новый объект от клиента и меняет состояние того объекта который лежит и ждёт у нас в куче
+            try
+            {
+                foreach (var prop in Parser(_obj?.GetType()))//это для отладки, чтобы видеть, что в куче есть наш объект со свойствами доступными для изменения и в них есть какое-то значение
+                {
+                    _logger.LogInformation($"Значение свойства {prop.Name} = " + prop.GetValue(_obj) + " объекта " + _obj?.GetType().Name + " до изменения");//это уже по сути для отладки, чтобы увидеть, что изменения произошли
+                }
+
+
+                foreach (var item in props.Keys)//собственно говоря меняем состояние объекта!
+                { 
+                    var value = (JsonElement)props[item];//апкастим до jsonElement, потому что именно такой тип всегда приходит к нам от клиента
+                    if (value.ValueKind == JsonValueKind.Number)
+                    {
+                        _obj?.GetType().GetProperty(item).SetValue(_obj, NumberConverter(props));
+                    }
+                    else {
+                        _obj?.GetType().GetProperty(item).SetValue(_obj, value.GetString());
+                    }
+                    
+                }
+
+                foreach (var prop in Parser(_obj?.GetType())) {//это уже по сути для отладки, чтобы увидеть, что изменения произошли
+                    _logger.LogInformation($"Значение свойства {prop.Name} = " + prop.GetValue(_obj) + " объекта " + _obj?.GetType().Name + " после изменения");
+                }
+
+                props.Add("Edited", true);//если нигде не возникло исключения, значит можно сигнплизировать клиенту, что объект успешно изменён
+                await Clients?.All.SendAsync("Recieve", props);//шлём обратно клиенту изменённый объект
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("При попытке изменения и отправки обратно клиенту объекта произошла ошибка: " + ex.Message + " stackTrace \n" + ex.StackTrace);//для разработчика
+                Clients?.All.SendAsync("Error", "Ошибка!", "При изменении свойств объекта на сервере, произошла ошибка, для того чтобы попробовать снова, обновите страничку");//для клиента
+            }
         }
         public override async Task OnConnectedAsync()
         {
@@ -69,7 +139,7 @@ namespace PropertyChanger.Hubs
         }
 
     }
-    class MyType1 {
+    class MyType1 {//собственно прототип нашего объекта, здесь может быть любой самописный тип
         public int MyIntProperty { get; set; } = 5;
         public string MyStringProperty { get; set; } = "Hello world";
     }
--- PropertyChanger/wwwroot/js/ChangerObjectClient.js
+++ PropertyChanger/wwwroot/js/ChangerObjectClient.js
@@ -1,6 +1,5 @@
-﻿let connection
-function ChangerObjectClient() {
-    connection = new signalR.HubConnectionBuilder()
+﻿function ChangerObjectClient() {
+    let connection = new signalR.HubConnectionBuilder()
         .withUrl("/Editor")
         .withAutomaticReconnect()
         .build();
@@ -10,12 +9,34 @@ function ChangerObjectClient() {
     }).catch(err => {
         console.error("Не получилось установить соединение " + err.toString())
     });
-    
+
+    connection.on("Error", (errorTitle, errorMessage) => {
+        console.error("Ошибка " + errorMessage);
+        $(document.forms.changerPostForm).html("");//чистим нашу форму перед новой отрисовкой
+
+        let formElements = "";
+        
+        formElements += `<div class="row mt-4">`
+        formElements += `<div class="col-3"><h5 style="color:red; text-align:left">${errorTitle}</h5></div>`
+        formElements += `</div>`
+        formElements += `<div class="row mt-2">`
+        formElements += `<div class="col-auto"><p style="color:red">${errorMessage}</p></div>`
+        formElements += `</div>`
+        
+        $(document.forms.changerPostForm).html(formElements);
+    })
 
     connection.on("Recieve", (data) => {
-        console.log(data)
+        console.log("Объект пришедший от сервера ")
+        console.log(data);
+
+        $(document.forms.changerPostForm).html("");//чистим нашу форму перед новой отрисовкой
+
         let formElements = "";
         for (let key in data) {//заполняем нашу форму
+            if (key == "Edited") {//нам не надо отрисовывать этот флаг в форме, если он есть
+                continue;
+            }
             formElements += `<div class="row mt-4" id="PropRow">`    
 
             formElements += `<label for=${key} class="col-sm-2">${key}</label>`;
@@ -23,11 +44,26 @@ function ChangerObjectClient() {
                 formElements += `<input type="text" class="form-control col-sm-2" value="${data[key]}">`;
             } else {
                 formElements += `<input type="number" class="form-control col-sm-2" value="${data[key]}">`;
-            }  
+            }
             formElements += `</div>` 
         }
-        formElements += `<div class="row mt-4"><div class="col-auto"> <input type="button" id="sendToServer" value="Отправить на сервер"></div></div>`;//добавляем кнопку в самом конце
+        if (data["Edited"]) {//проверяем наличие флага в присланном объекте, если он есть, то значит сигнализируем, что объект изменён
+            formElements += `<div class="row mt-4"><div class="col-auto"> <input type="button" id="sendToServer" value="Изменить объект"><p style="color:green">${data["Edited"]==true?"Объект успешно изменён":""}</p></div></div>`;//добавляем кнопку в самом конце
+        } else {
+            formElements += `<div class="row mt-4"><div class="col-auto"> <input type="button" id="sendToServer" value="Изменить объект"></div></div>`;//добавляем кнопку в самом конце
+        }
+        
         $(document.forms.changerPostForm).html(formElements);
+
+        $('input[type="number"]').on('change keyup', function () {//так как дурацкие браузеры позволяют писать буквы в инпутах с типом намбер, вешаем реплейсеры
+            // Remove invalid characters
+            var sanitized = $(this).val().replace(/[^-0-9]/g, '');
+            // Remove non-leading minus signs
+            sanitized = sanitized.replace(/(.)-+/g, '$1');
+            // Update value
+            $(this).val(sanitized);
+        });
+
 
         $("input#sendToServer").click(() => {//вешаем на кнопку обработчик
             let rows = $("form#changerPostForm>div#PropRow");
@@ -35,15 +71,20 @@ function ChangerObjectClient() {
             for (let i = 0; i < rows.length; i++) {
                 let name = rows[i].children[0].innerText;
                 let value;
-                if (!isNaN(Number(rows[i].children[1].value))) {//фильтруем намберы и стринги, тоесть если не удаётся преобразовать в намбер, то значит это стринг
+                if (!isNaN(Number(rows[i].children[1].value)) && Number(rows[i].children[1].value!="")) {//фильтруем намберы и стринги, тоесть если не удаётся преобразовать в намбер, то значит это стринг
                     value = Number(rows[i].children[1].value);
                 } else {
-                    value = rows[i].children[1].value;
+                    value = rows[i].children[1].value.toString();
                 }
                               
                 dict[`${name}`] = value;
             }
+            console.log("Объект отправляемый серверу ");
             console.log(dict);
+
+            connection.invoke("Recieve", dict).catch(err => {
+                console.error("Не получилось отправить изменённый объект на сервер по причине " + err.toString());
+            })
         })
     })
 
